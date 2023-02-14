27c34b01247ec68583ee82d72a5f86518a748e27
PioR <roondi94@gmail.com>
2023-02-14 05:24:25 +0100

    I removed the options to roll and collapse the item.

--- src/components/OneDoctor/OneDoctor.tsx
+++ src/components/OneDoctor/OneDoctor.tsx
@@ -3,6 +3,7 @@ import './OneDoctor.css'
 import {Btn} from "../Btn/Btn";
 import {FreeTermHour} from "../FreeTermHour/FreeTermHour";
 import {FreeTermDay} from "../FreeTermDay/FreeTermDay";
+import {FreeTermWeek} from "../FreeTermWeek/FreeTermWeek";
 
 
 interface Props {
@@ -13,34 +14,17 @@ interface Props {
     specialization: string;
 
 
-
 }
 
 export const OneDoctor = (props: Props) => {
 
-    const [on, setOn] = useState(false);
 
 
-
- const onClick = () => {
-     setOn(true);
-     if(on) setOn(false);
-    }
-
+    return <div className="divVisit">
+            <p className="pDr">Dr {props.name} {props.lastName} spec. {props.specialization}  </p>
+            <FreeTermWeek idDr={props.idDr}/>
+        </div>
 
 
-
-return on ? <div className="divVisit">
-        <p className="pDr">Dr {props.name} {props.lastName} spec. {props.specialization}  </p> <button className="btnDr" onClick={onClick}>Wróć</button>
-        <FreeTermDay numberDay={'6'} month={'Lut'} year={'2023'} idDr={'f21779ec-b1bd-4a2c-afbc-7f614a75a19d'}/>
-
-         <hr className="hrVisit"/>
-    </div>
-
-          : <div className="divDr">
-        <p className="pDr">Dr {props.name} {props.lastName} spec. {props.specialization} </p> <button className="btnDr" onClick={onClick}>Zarezerwuj wizytę</button>
-        <hr/>
-
-    </div>
 
 }
