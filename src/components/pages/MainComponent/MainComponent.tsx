ac55baacbadda241a69121f5fb6fa4f1135bd86c
PioR <roondi94@gmail.com>
2023-06-25 13:45:55 +0200

    star site responsiveness

--- src/components/pages/MainComponent/MainComponent.tsx
+++ src/components/pages/MainComponent/MainComponent.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import "./MainComponent.css"
-
+import { Link } from 'react-router-dom';
+import './MainComponent.css';
 
 export const MainComponent = () => {
+  return (
+    <>
+      <div className="main-bg">
+        <div className="logo" />
+        <header className="main-header"></header>
 
-    return <>
-        <div className="bg">
-            <div className="center">
-                <h2>Jestem</h2>
-                <Link to="patient" className="link1">Pacjentem</Link> <br/>
-                <Link to="doctor" className="link2">Lekarzem</Link>
-            </div>
+        <div className="slogan">
+          <h2 className="slogan-h2 slogan-h2-up ">Opieka Zdrowotna Online </h2>
+          <h2 className="slogan-h2 slogan-h2-down">Proste Umawianie </h2>
+          <h2 className="slogan-h2 slogan-h2-third">Profesjonalizm</h2>
+          <h3 className="slogan-h3">Znajdź najlepszego lekarza w Twojej okolicy i umów się na wizytę bez zbędnych kolejek i stresu.</h3>
         </div>
+        <div className="png" />
+      </div>
     </>
-
-}
+  );
+};
