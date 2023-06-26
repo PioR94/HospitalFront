e1c46daa2c10efa483035c586b453bd915f9dc12
PioR <roondi94@gmail.com>
2023-06-26 20:39:27 +0200

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import "./App.css";
-import {} from "types";
-import { CreateDoctorForm } from "./components/molecules/CreateDoctorForm/CreateDoctorForm";
-import { Link, Route, Routes } from "react-router-dom";
-import { CreatePatientForm } from "./components/molecules/CreatePatientForm/CreatePatientForm";
-import { BrowserRouter } from "react-router-dom";
-import { MainComponent } from "./components/pages/MainComponent/MainComponent";
-import { LoginPatient } from "./components/molecules/LoginPatient/LoginPatient";
-import { LoginDoctor } from "./components/molecules/LoginDoctor/LoginDoctor";
+import React from 'react';
+import './App.css';
+import {} from 'types';
+import { CreateDoctorForm } from './components/molecules/CreateDoctorForm/CreateDoctorForm';
+import { Link, Route, Routes } from 'react-router-dom';
+import { CreatePatientForm } from './components/molecules/CreatePatientForm/CreatePatientForm';
+import { BrowserRouter } from 'react-router-dom';
+import { MainComponent } from './components/pages/MainComponent/MainComponent';
+import { LoginPatient } from './components/molecules/LoginPatient/LoginPatient';
+import { LoginDoctor } from './components/molecules/LoginDoctor/LoginDoctor';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 export default function App() {
   return (
--- src/components/pages/MainComponent/MainComponent.tsx
+++ src/components/pages/MainComponent/MainComponent.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MainComponent.css';
+import { Btn } from '../../atoms/Btn/Btn';
+import { Dropdown } from 'primereact/dropdown';
 
 export const MainComponent = () => {
+  const [selectedCity, setSelectedCity] = useState(null);
+  const cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' },
+  ];
   return (
     <>
       <div className="main-bg">
         <div className="logo" />
-        <header className="main-header"></header>
+        <header className="main-header">
+          <div className="card flex justify-content-center" id="check-logout">
+            <Dropdown
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.value)}
+              options={cities}
+              optionLabel="name"
+              placeholder="Zaloguj"
+              className="w-full md:w-14rem"
+            />
+          </div>
+          <div className="card flex justify-content-center" id="check-register">
+            <Dropdown
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.value)}
+              options={cities}
+              optionLabel="name"
+              placeholder="Rejestracja"
+              className="w-full md:w-14rem"
+              style={{ border: 'none' }}
+            />
+          </div>
+        </header>
 
         <div className="slogan">
           <h2 className="slogan-h2 slogan-h2-up ">Opieka Zdrowotna Online </h2>
