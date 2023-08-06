cc528ae01b5494a913be2003c5889baa80161018
PioR <roondi94@gmail.com>
2023-08-06 11:13:57 +0200

    add selectors.ts

---
+++ src/redux/selectors.ts
@@ -0,0 +1,5 @@
+// import { UserState } from '../types/redux/user-state';
+
+export const selectActiveLogin = (state: any) => state.user.activeLogin;
+
+export const selectUserId = (state: any) => state.user.id;
