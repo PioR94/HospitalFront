c3d94912a4fd8b2d454f5dbc32e33a934d7abde4
Piotr <piotr_r@tutanota.com>
2023-10-15 21:14:46 +0200

    complete user form

--- src/components/molecules/CreateUserForm/CreateUserForm.tsx
+++ src/components/molecules/CreateUserForm/CreateUserForm.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useEffect, useRef, useState } from 'react';
 import './CreateUserForm.css';
-import { baseUrlDoctor, baseUrlSpecialization, downloadData, sendAndReceiveData } from '../../../api';
+import { baseUrlSpecialization, downloadData, sendAndReceiveData } from '../../../api';
 import { Messages } from 'primereact/messages';
 import { createValidation, MESS_OK, showInfoMessage } from '../../../utils/functions/create-validation';
 import { Dropdown } from 'primereact/dropdown';
@@ -9,6 +9,7 @@ import { Password } from 'primereact/password';
 import { InputMask } from 'primereact/inputmask';
 import { Role } from '../../../types/role/role';
 import { chooseValue } from '../../../utils/functions/choose-value';
+import { Button } from 'primereact/button';
 
 export const CreateUserForm = (props: Role) => {
   const [message, setMessage] = useState({
@@ -74,53 +75,64 @@ export const CreateUserForm = (props: Role) => {
 
   return (
     <>
-      <div className="cc">
-        <form onSubmit={sendForm} className="formRegister">
-          <h2>Rejestracja</h2>
-          <div className="card flex justify-content-center">
-            <label htmlFor="login">Login</label>
-            <InputText type="text" id="login" value={form.login} onChange={(e) => updateForm('login', e.target.value)} />{' '}
+      <div className="container-CUF">
+        <form onSubmit={sendForm} className="form-register">
+          <h2 id="register-h2">Rejestracja</h2>
+          <div className="container-div">
+            <div className="container-input">
+              <label htmlFor="login">Login</label>
+              <InputText type="text" id="login" value={form.login} onChange={(e) => updateForm('login', e.target.value)} />{' '}
+            </div>
+            <div className="container-input">
+              <label htmlFor="mail">Email</label>
+              <InputText type="email" id="mail" value={form.mail} onChange={(e) => updateForm('mail', e.target.value)} />
+            </div>
           </div>
-          <div>
+          <div className="container-input" id="password-div">
             <label htmlFor="password">Hasło</label>
-            <Password type="password" id="password" value={form.password} onChange={(e) => updateForm('password', e.target.value)} toggleMask />
+            <Password
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={(e) => updateForm('password', e.target.value)}
+              toggleMask
+              style={{ width: '80% !important', border: 'none' }}
+            />
           </div>
-          <div>
-            <label htmlFor="mail">Email</label>
+          <div className="container-div">
+            <div className="container-input">
+              <label htmlFor="">Imię</label>
+              <InputText type="text" id="name" value={form.name} onChange={(e) => updateForm('name', e.target.value)} />
+            </div>
+            <div className="container-input">
+              <label htmlFor="lastName">Nazwisko</label>
 
-            <InputText type="email" id="mail" value={form.mail} onChange={(e) => updateForm('mail', e.target.value)} />
+              <InputText type="text" id="lastName" value={form.lastName} onChange={(e) => updateForm('lastName', e.target.value)} />
+            </div>
           </div>
-          <div>
-            <label htmlFor="">Imię</label>
-
-            <InputText type="text" id="name" value={form.name} onChange={(e) => updateForm('name', e.target.value)} />
-          </div>
-          <div>
-            <label htmlFor="lastName">Nazwisko</label>
-
-            <InputText type="text" id="lastName" value={form.lastName} onChange={(e) => updateForm('lastName', e.target.value)} />
-          </div>
-          <div>
+          <div className="container-input">
             <label htmlFor="street">Ulica</label>
             <InputText type="text" id="street" value={form.street} onChange={(e) => updateForm('street', e.target.value)} />
           </div>
-          <div>
-            <label htmlFor="code">Kod pocztowy</label>
-            <InputMask
-              type="text"
-              id="code"
-              value={form.code}
-              onChange={(e) => updateForm('code', e.target.value)}
-              mask="99-999"
-              placeholder="__-___"
-            />
-          </div>
-          <div>
-            <label htmlFor="city">Miasto</label>
-            <InputText type="text" id="city" value={form.city} onChange={(e) => updateForm('city', e.target.value)} />
+          <div className="container-div">
+            <div className="container-input">
+              <label htmlFor="code">Kod pocztowy</label>
+              <InputMask
+                type="text"
+                id="code"
+                value={form.code}
+                onChange={(e) => updateForm('code', e.target.value)}
+                mask="99-999"
+                placeholder="__-___"
+              />
+            </div>
+            <div className="container-input">
+              <label htmlFor="city">Miasto</label>
+              <InputText type="text" id="city" value={form.city} onChange={(e) => updateForm('city', e.target.value)} />
+            </div>
           </div>
           {props.role === 'doctor' ? (
-            <div>
+            <div className="container-input">
               <label htmlFor="specialization">Specializacja</label>
               <Dropdown
                 value={form.specialization}
@@ -132,8 +144,7 @@ export const CreateUserForm = (props: Role) => {
               />
             </div>
           ) : null}
-
-          <button className="formRegister-button">Wyslij</button>
+          <Button className="send-form" label="Wyślij" icon="pi pi-check" />
         </form>
         <Messages className="messages" ref={msgs} />
       </div>
