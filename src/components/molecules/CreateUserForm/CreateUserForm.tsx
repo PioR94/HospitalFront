db1cdf8710e98a3c791066bc4a0b6e365bf59e49
Piotr <piotr_r@tutanota.com>
2023-10-27 20:26:29 +0200

    change form to react hook form in CreateUserForm

--- src/components/molecules/CreateUserForm/CreateUserForm.tsx
+++ src/components/molecules/CreateUserForm/CreateUserForm.tsx
@@ -1,8 +1,7 @@
-import React, { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import React, { BaseSyntheticEvent, SyntheticEvent, useEffect, useRef, useState } from 'react';
 import './CreateUserForm.css';
 import { baseUrlSpecialization, downloadData, sendAndReceiveData } from '../../../api';
 import { Messages } from 'primereact/messages';
-import { createValidation, MESS_OK, showInfoMessage } from '../../../utils/functions/create-validation';
 import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
@@ -10,143 +9,175 @@ import { InputMask } from 'primereact/inputmask';
 import { Role } from '../../../types/role/role';
 import { chooseValue } from '../../../utils/functions/choose-value';
 import { Button } from 'primereact/button';
+import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import { InputsAddForm, InputsLog } from '../../../types/hook-form/inputs';
 
 export const CreateUserForm = (props: Role) => {
-  const [message, setMessage] = useState({
-    id: 0,
-    message: '',
-  });
   const [specializations, setSpecializations] = useState([]);
-  const msgs = useRef<Messages>(null);
+  const [comparePassword, setComparePassword] = useState<boolean>(true);
   const url = chooseValue(props.role) || '';
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<InputsAddForm>();
 
+  const [password, repeatPassword] = watch(['password', 'repeatPassword']);
   useEffect(() => {
-    if (message.message === MESS_OK) {
-      sendAndReceiveData(form, url, 'ad').then((r) => {
-        console.log(r);
-        if (r.login) {
-          setMessage({
-            id: Date.now(),
-            message: MESS_OK,
-          });
-          showInfoMessage(msgs, 'success', 'Ok', message.message);
-        }
-      });
-    } else if (message.message && message.message !== MESS_OK) {
-      showInfoMessage(msgs, 'error', 'Błąd', message.message);
-    }
-    console.log(props.role);
-    console.log(chooseValue(props.role));
-  }, [message]);
+    setComparePassword(password === repeatPassword);
+  }, [password, repeatPassword]);
 
-  const [form, setForm] = useState({
-    login: '',
-    password: '',
-    mail: '',
-    name: '',
-    lastName: '',
-    street: '',
-    code: '',
-    city: '',
-    specialization: undefined,
-  });
   useEffect(() => {
     downloadData(baseUrlSpecialization).then((r) => {
       setSpecializations(r);
     });
   }, []);
 
-  const updateForm = (key: string, value: any) => {
-    setForm((form) => ({
-      ...form,
-      [key]: value,
-    }));
-  };
-
-  const sendForm = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    console.log(form);
-
-    setMessage({
-      message: createValidation(form.login, form.password, form.name, form.lastName, form.street, form.code, form.city, form.specialization),
-      id: Date.now(),
-    });
+  const onSubmit: SubmitHandler<InputsLog> = (data: InputsLog, event: BaseSyntheticEvent | undefined) => {
+    event?.preventDefault();
+    console.log(data);
+    sendAndReceiveData(data, url, 'add').then((data) => {});
   };
 
   return (
     <>
       <div className="container-CUF">
-        <form onSubmit={sendForm} className="form-register">
+        <form onSubmit={handleSubmit(onSubmit)} className="form-register">
           <h2 id="register-h2">Rejestracja</h2>
           <div className="container-div">
             <div className="container-input">
-              <label htmlFor="login">Login</label>
-              <InputText type="text" id="login" value={form.login} onChange={(e) => updateForm('login', e.target.value)} />{' '}
+              <label htmlFor="login" style={errors.login && { color: 'red' }}>
+                Login
+              </label>
+              <InputText type="text" id="login" {...register('login', { required: true })} style={errors.login && { borderColor: 'red' }} />
+              {errors.login && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
             <div className="container-input">
-              <label htmlFor="mail">Email</label>
-              <InputText type="email" id="mail" value={form.mail} onChange={(e) => updateForm('mail', e.target.value)} />
+              <label htmlFor="mail" style={errors.mail && { color: 'red' }}>
+                Email
+              </label>
+              <InputText type="email" id="mail" {...register('mail', { required: true })} style={errors.mail && { borderColor: 'red' }} />
+              {errors.mail && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
           </div>
-          <div className="container-input" id="password-div">
-            <label htmlFor="password">Hasło</label>
-            <Password
-              type="password"
-              id="password"
-              value={form.password}
-              onChange={(e) => updateForm('password', e.target.value)}
-              toggleMask
-              style={{ width: '80% !important', border: 'none' }}
-            />
+          <div className="container-div">
+            <div className="container-input" id="password-div">
+              <label htmlFor="password" style={errors.password && { color: 'red' }}>
+                Hasło
+              </label>
+              <InputText
+                id="password"
+                type="password"
+                {...register('password', {
+                  required: true,
+                  validate: {
+                    checkLength: (value) => value.length >= 8,
+                  },
+                })}
+                style={errors.password && { borderColor: 'red' }}
+              />
+              {errors.password?.type === 'required' && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
+              {errors.password?.type === 'checkLength' && <p style={{ color: 'red', fontSize: 12 }}>Hasło musi zawierać min 8 znaków</p>}
+            </div>
+            <div className="container-input" id="password-div">
+              <label htmlFor="repeat-password" style={errors.repeatPassword && { color: 'red' }}>
+                Powtórz Hasło
+              </label>
+              <InputText
+                id="repeat-password"
+                type="password"
+                // feedback={false}
+                // toggleMask
+                {...register('repeatPassword', {
+                  required: true,
+                  validate: {
+                    compare: (value) => value === password,
+                  },
+                })}
+                style={errors.repeatPassword && { borderColor: 'red' }}
+              />
+              {errors.repeatPassword?.type === 'required' && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
+              {errors.repeatPassword?.type === 'compare' && (
+                <p style={{ color: 'red', fontSize: 12 }}>Wartość pola jest inna od wartości w polu hasło</p>
+              )}
+            </div>
           </div>
           <div className="container-div">
             <div className="container-input">
-              <label htmlFor="">Imię</label>
-              <InputText type="text" id="name" value={form.name} onChange={(e) => updateForm('name', e.target.value)} />
+              <label htmlFor="name" style={errors.name && { color: 'red' }}>
+                Imię
+              </label>
+              <InputText type="text" id="name" {...register('name', { required: true })} style={errors.name && { borderColor: 'red' }} />
+              {errors.name && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
             <div className="container-input">
-              <label htmlFor="lastName">Nazwisko</label>
+              <label htmlFor="lastName" style={errors.lastName && { color: 'red' }}>
+                Nazwisko
+              </label>
 
-              <InputText type="text" id="lastName" value={form.lastName} onChange={(e) => updateForm('lastName', e.target.value)} />
+              <InputText type="text" id="lastName" {...register('lastName', { required: true })} style={errors.lastName && { borderColor: 'red' }} />
+              {errors.lastName && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
-          </div>
-          <div className="container-input">
-            <label htmlFor="street">Ulica</label>
-            <InputText type="text" id="street" value={form.street} onChange={(e) => updateForm('street', e.target.value)} />
           </div>
           <div className="container-div">
             <div className="container-input">
-              <label htmlFor="code">Kod pocztowy</label>
+              <label htmlFor="code" style={errors.code && { color: 'red' }}>
+                Kod pocztowy
+              </label>
               <InputMask
                 type="text"
                 id="code"
-                value={form.code}
-                onChange={(e) => updateForm('code', e.target.value)}
                 mask="99-999"
                 placeholder="__-___"
+                {...register('code', { required: true })}
+                style={errors.code && { borderColor: 'red' }}
               />
+              {errors.code && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
             <div className="container-input">
-              <label htmlFor="city">Miasto</label>
-              <InputText type="text" id="city" value={form.city} onChange={(e) => updateForm('city', e.target.value)} />
+              <label htmlFor="city" style={errors.city && { color: 'red' }}>
+                Miasto
+              </label>
+              <InputText type="text" id="city" {...register('city', { required: true })} style={errors.city && { borderColor: 'red' }} />
+              {errors.city && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
           </div>
+          <div className="container-input">
+            <label htmlFor="street" style={errors.street && { color: 'red' }}>
+              Ulica
+            </label>
+            <InputText type="text" id="street" {...register('street', { required: true })} style={errors.street && { borderColor: 'red' }} />
+            {errors.street && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
+          </div>
+
           {props.role === 'doctor' ? (
             <div className="container-input">
-              <label htmlFor="specialization">Specializacja</label>
-              <Dropdown
-                value={form.specialization}
-                onChange={(e) => updateForm('specialization', e.target.value)}
-                options={specializations}
-                placeholder="Wybierz specjalizację"
-                className="w-full md:w-14rem"
-                id="specialization"
+              <label htmlFor="specialization" style={errors.specialization && { color: 'red' }}>
+                Specializacja
+              </label>
+              <Controller
+                name="specialization"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Dropdown
+                    {...field}
+                    options={specializations}
+                    placeholder="Wybierz specjalizację"
+                    id="specialization"
+                    {...register('specialization', { required: true })}
+                    style={errors.specialization && { borderColor: 'red', color: 'red' }}
+                  />
+                )}
               />
+
+              {errors.specialization && <p style={{ color: 'red', fontSize: 12 }}>Pole jest wymagane</p>}
             </div>
           ) : null}
-          <Button className="send-form" label="Wyślij" icon="pi pi-check" />
+          <Button className="send-form" label="Wyślij" icon="pi pi-check" style={{ minHeight: 40 }} />
         </form>
-        <Messages className="messages" ref={msgs} />
       </div>
     </>
   );
--- src/types/hook-form/inputs.ts
+++ src/types/hook-form/inputs.ts
@@ -2,3 +2,16 @@ export type InputsLog = {
   login: string;
   password: string;
 };
+
+export type InputsAddForm = {
+  login: string;
+  mail: string;
+  password: string;
+  repeatPassword: string;
+  name: string;
+  lastName: string;
+  street: string;
+  code: string;
+  city: string;
+  specialization?: string;
+};
