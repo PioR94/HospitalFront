123a04e937c28ef2dc09f46ff516a30ff2333a5b
Piotr <piotr_r@tutanota.com>
2024-02-16 22:16:58 +0100

    working week

--- src/hooks/components/Week/useInitializeData.ts
+++ src/hooks/components/Week/useInitializeData.ts
@@ -1,16 +1,18 @@
 import { useEffect } from 'react';
 import { baseUrlSchedule, sendAndReceiveData } from '../../../api';
 import { addReduxHours } from '../../../redux/schedule-slice';
-import { useAppDispatch } from '../../common/redux';
+import { useAppDispatch, useAppSelector } from '../../common/redux';
+import { RootState } from '../../../redux/store';
 
 export const useInitializeData = (idDr: string) => {
+  const reduxHours = useAppSelector((state: RootState) => state.schedule.hours);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (idDr) {
+    if (idDr && reduxHours.length === 0) {
       sendAndReceiveData(idDr, baseUrlSchedule, 'hours').then((r) => {
         dispatch(addReduxHours(r));
       });
     }
-  }, [idDr, dispatch]);
+  }, []);
 };
